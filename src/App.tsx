331a4fbda8ec79f7f9c411ea0f7e208e0bd18fa9
seunosinowo <oluwaseunpaul98@gmail.com>
2025-01-08 15:33:45 +0100

    Implemented parts of NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contact Us",
+}
+
+
 function App() {
-  return <div className="app">app</div>;
+  const[selectedPage, setSelectedPage] = useState("home")
+
+  return  (
+  <div className="app bg-gray-20">
+    <Navbar selectedPage = {selectedPage} setSelectedPage = {setSelectedPage}/>
+    
+  </div>
+  )
 }
 
 export default App;
